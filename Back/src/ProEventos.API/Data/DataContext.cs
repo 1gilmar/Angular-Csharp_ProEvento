3c5e5ebc455234f8b7713ad20835c9fa3f7803cb
1gilmar <1gilmar@gmail.com>
2021-03-03 12:33:41 -0300

    Paramento na criacao do contexto ficou faltando.

--- Back/src/ProEventos.API/Data/DataContext.cs
+++ Back/src/ProEventos.API/Data/DataContext.cs
@@ -5,6 +5,7 @@ namespace ProEventos.API.Data
 {
     public class DataContext : DbContext
     {
+        public DataContext(DbContextOptions<DataContext> options) :base(options) {}
         public DbSet<Evento> Eventos { get; set; }
         
     }
