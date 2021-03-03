f6e1c7f27e98eaa1987dadd1dbde8fc1a0503eeb
1gilmar <1gilmar@gmail.com>
2021-03-03 10:15:01 -0300

    Exemplo dos 4 metodos http

---
+++ Back/src/ProEventos.API/Controllers/EventoController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace ProEventos.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class EventoController : ControllerBase
+    {
+        public EventoController(){
+            
+        }
+
+        [HttpGet]
+        public string Get()
+        {
+            return "Exempolo de get";
+        }
+
+        [HttpPost]
+        public string Post()
+        {
+            return "Exemplo de post";
+        }
+
+        [HttpPut("{id}")]
+        public string Put(int id)
+        {
+            return $"Exemplo de Put = {id}";
+        }
+
+        [HttpDelete("{id}")]
+        public string Delete(int id)
+        {
+            return $"Exemplo de Delete = {id}";
+        }
+    }
+}
