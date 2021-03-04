0b176d0249f1880ca1560cec7ba49de5673e38f5
1gilmar <1gilmar@gmail.com>
2021-03-04 10:42:51 -0300

    Ocultar e exibir a coluna de imagem, usando ngif e colaped

--- Front/ProEventos/src/app/eventos/eventos.component.ts
+++ Front/ProEventos/src/app/eventos/eventos.component.ts
@@ -10,12 +10,18 @@ export class EventosComponent implements OnInit {
 
   eventos: any = []
   //tamanho da imagem
-  widthImg = 50
+  larguraImagem: number = 150
+  margemImagem = 2
+  exibirImagem = true
 
   constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
     this.getEventos()
+  }
+
+  alterarImagem(){
+    this.exibirImagem = !this.exibirImagem
   }
 
   getEventos(): void {
