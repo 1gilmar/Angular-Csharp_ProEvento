e32e823c5eacbe7526904a7ac03e9ccc5d12df3d
1gilmar <1gilmar@gmail.com>
2021-03-05 14:26:03 -0300

    Ajustado a referencia para o migrate reconhecer o MigrationBuilder

--- Back/src/ProEventos.Domain/RedeSocial.cs
+++ Back/src/ProEventos.Domain/RedeSocial.cs
@@ -2,7 +2,7 @@ namespace ProEventos.Domain
 {
     public class RedeSocial
     {
-        public int It { get; set; }
+        public int Id { get; set; }
         public string Nome { get; set; }
         public string URL { get; set; }
         
