67bb3bed11ef425f2182f65b53a52b43a8fff501
1gilmar <1gilmar@gmail.com>
2021-03-13 16:00:25 -0300

    Angular - Tooltip e Dropdown

--- Front/ProEventos/src/app/app.module.ts
+++ Front/ProEventos/src/app/app.module.ts
@@ -1,15 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule } from '@angular/common/http'
+import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { CollapseModule } from 'ngx-bootstrap/collapse';
+import { TooltipModule } from 'ngx-bootstrap/tooltip';
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { EventosComponent } from './eventos/eventos.component';
 import { PalestrantesComponent } from './palestrantes/palestrantes.component';
 import { NavComponent } from './nav/nav.component';
-import { CollapseModule } from 'ngx-bootstrap/collapse'
+
 import { EventoService } from 'src/services/evento.service';
 import { DateTimeFormatePipe } from 'src/helpers/DateTimeFormate.pipe';
 
@@ -23,11 +28,14 @@ import { DateTimeFormatePipe } from 'src/helpers/DateTimeFormate.pipe';
    ],
   imports: [
     BrowserModule,
+    FormsModule,
     AppRoutingModule,
     HttpClientModule,
     BrowserAnimationsModule,
     CollapseModule.forRoot(),
-    FormsModule
+    TooltipModule.forRoot(),
+    BsDropdownModule.forRoot()
+
   ],
   providers: [EventoService],
   bootstrap: [AppComponent]
