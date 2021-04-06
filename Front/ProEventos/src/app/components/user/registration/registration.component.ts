d504116c4a3cc2e07d4b1aa55f2e2dd70aa5fa1f
1gilmar <1gilmar@gmail.com>
2021-04-06 15:16:56 -0300

    Angular - Form Registration

--- Front/ProEventos/src/app/components/user/registration/registration.component.ts
+++ Front/ProEventos/src/app/components/user/registration/registration.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-registration',
@@ -7,9 +8,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegistrationComponent implements OnInit {
 
-  constructor() { }
+  form!: FormGroup;
+
+  get f(): any {
+    return this.form.controls;
+  }
+
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit(): void {
+    this.validation();
+  }
+
+  private validation(): void{
+    this.form = this.fb.group({
+      primeiroNome: ['', Validators.required],
+      ultimoNome: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      userName: ['', Validators.required],
+      senha: ['', [Validators.required, Validators.minLength(6)]],
+      confirmeSenha: ['', Validators.required],
+    });
   }
 
 }
